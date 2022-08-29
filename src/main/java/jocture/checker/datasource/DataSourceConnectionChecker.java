bbdeebd22eaff77f1fba37dc0d6f226b9b3aab94
yamsroun <yamsroun@gmail.com>
2022-08-29 22:06:33 +0900

    63번째 - 체커 애플리케이션 (1)

---
+++ src/main/java/jocture/checker/datasource/DataSourceConnectionChecker.java
@@ -0,0 +1,45 @@
+package jocture.checker.datasource;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DataSourceConnectionChecker implements ExitCodeGenerator {
+
+    private final DataSourceProperties dataSourceProperties;
+    private int exitCode = 0;
+
+    @Override
+    public int getExitCode() {
+        return exitCode;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void checkConnections() {
+        List<ConnectionProperties> list = dataSourceProperties.getList();
+        list.forEach(prop -> {
+            log.debug("conn={}", prop);
+            // 커넥션 생성
+            // 커넥션 : 실제 물리 DB와 연결 세션 (DataSource를 통해 커넥션을 획득한다.)
+            // 커넥션 연결 방법 : DataSource를 통해서 합니다.
+            DataSource dataSource = new DriverManagerDataSource(prop.getUrl(), prop.getUsername(), prop.getPassword());
+            try (Connection conn = dataSource.getConnection()) {
+                log.info("SUCCESS : {} ({})", prop.getName(), prop.getUrl());
+            } catch (Exception e) {
+                log.error("FAIL : {} ({})", prop.getName(), prop.getUrl());
+                exitCode = 1;
+            }
+        });
+    }
+}
---
+++ src/main/java/jocture/checker/datasource/DataSourceProperties.java
@@ -0,0 +1,16 @@
+package jocture.checker.datasource;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Setter @Getter
+@ConfigurationProperties(prefix = "spring.datasource")
+public class DataSourceProperties {
+
+    private List<ConnectionProperties> list;
+}
